385a710e5f8c430631f8fda5f0cc26729de43c68
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-11-22 15:47:15 +0100

    add entity Person

--- src/main/java/com/example/marocship_1/entity/Person.java
+++ src/main/java/com/example/marocship_1/entity/Person.java
@@ -1,7 +1,111 @@
 package com.example.marocship_1.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.time.Instant;
 
 @Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "nom", nullable = false, length = 50)
+    private String nom;
+
+    @Column(name = "prenom", nullable = false, length = 50)
+    private String prenom;
+
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+
+    @Lob
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @OneToOne(mappedBy = "person")
+    private Chauffeur chauffeur;
+
+    @OneToOne(mappedBy = "person")
+    private Manager manager;
+
+    @OneToOne(mappedBy = "person")
+    private Responsable responsable;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Chauffeur getChauffeur() {
+        return chauffeur;
+    }
+
+    public void setChauffeur(Chauffeur chauffeur) {
+        this.chauffeur = chauffeur;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    public Responsable getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(Responsable responsable) {
+        this.responsable = responsable;
+    }
+
 }
