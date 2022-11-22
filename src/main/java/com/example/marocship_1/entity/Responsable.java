85ee4f68a9ce3237803bfc1f7565b09edc65fcfe
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-11-22 15:47:16 +0100

    add entity Responsable

--- src/main/java/com/example/marocship_1/entity/Responsable.java
+++ src/main/java/com/example/marocship_1/entity/Responsable.java
@@ -1,7 +1,32 @@
 package com.example.marocship_1.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class Responsable {
+    @Id
+    @Column(name = "id_Responsable", nullable = false)
+    private Integer id;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_Responsable", nullable = false)
+    private Person person;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
 }
